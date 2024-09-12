31b8c912c0f1f6c6e5cfb7158955f33e0a5523be
Oghenevwede Louis Umukoro <wedevilleg@gmail.com>
2024-09-12 05:08:41 +0100

    swiper

--- src/components/HomePage/HomePageFour.jsx
+++ src/components/HomePage/HomePageFour.jsx
@@ -1,5 +1,17 @@
 'use client'
 
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 import Image from "next/image"
 import Link from "next/link"
 import { useState,useEffect } from "react"
@@ -153,6 +165,37 @@ const HomePageFour=()=>{
                     </div>
 
                     {/* swiper */}
+
+                    <>
+                        <Swiper
+                            slidesPerView={3}
+                            spaceBetween={30}
+                             pagination={{
+                            clickable: true,
+                            }}
+                             modules={[Pagination]}
+                            className="mySwiper"
+      >
+                            <SwiperSlide>
+                                <div>
+                                        <div>
+                                            <h1></h1>
+                                        </div>
+                                        <div></div>
+                                </div>
+                            </SwiperSlide>
+
+
+                            <SwiperSlide>Slide 2</SwiperSlide>
+                            <SwiperSlide>Slide 3</SwiperSlide>
+                            <SwiperSlide>Slide 4</SwiperSlide>
+                             <SwiperSlide>Slide 5</SwiperSlide>
+                             <SwiperSlide>Slide 6</SwiperSlide>
+                            <SwiperSlide>Slide 7</SwiperSlide>
+                            <SwiperSlide>Slide 8</SwiperSlide>
+                             <SwiperSlide>Slide 9</SwiperSlide>
+                        </Swiper>
+                    </>
                     
 
                 </div>
